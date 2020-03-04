dbef9dffb48639684e5eb1e001467cb688f6fcc8
Amanda Mills-Hubbard <amanda.millshubbard@gmail.com>
2020-03-03 23:40:59 -0500

    Zero to Hero

---
+++ zeros.js
@@ -0,0 +1,21 @@
+function noBoringZeros(n) {
+  // your code
+ let noZero = Number(n.toString().replace(/0+$/,''));
+ return (noZero);
+}
+
+
+
+function testing(actual, expected) {
+    Test.assertSimilar(actual, expected);
+}
+
+Test.describe("Basic tests",function() {
+Test.it("noBoringZeros",function() {
+    testing(noBoringZeros(1450), 145)
+    testing(noBoringZeros(960000), 96)
+    testing(noBoringZeros(1050), 105)
+    testing(noBoringZeros(-1050), -105)
+    testing(noBoringZeros(-105), -105)
+    testing(noBoringZeros(0), 0)
+})})
